3dcdc90fff76cd43bb27ea97f72fd0fcbd07126e
Muhammad Faayeq Ali Khan <faayeqali@gmail.com>
2025-01-10 04:49:57 +0500

    First commit.

---
+++ assembler.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void tokenise_line(const char* line){
+   
+}
+
+int main(int argc, char *argv[]) {
+    if(argc!=2){
+        printf("Using: %s <f_name>\n, argv[0]");
+        return 1;
+    }
+    const char* f_name = argv[1];
+
+    FILE *file = fopen(f_name, "r");
+    if(file==NULL){
+       perror("Can't open file for reading");
+       return 1; 
+    }
+
+    char line_buffer[256];
+}
