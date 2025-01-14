52411906445f81c2f2f1b8fcf88b3953ba9e10ec
Muhammad Faayeq Ali Khan <faayeqali@gmail.com>
2025-01-14 08:19:29 +0500

    formatted

--- src/assembler.c
+++ src/assembler.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAP_SIZE 100
+#define MAP_SIZE	  100
 #define MAX_CODE_SIZE 1024
 #define MAX_DATA_SIZE 512
 
@@ -12,13 +12,11 @@ typedef struct sym_pair {
 	struct sym_pair *next;
 } sym_pair;
 
-
 typedef struct sym_table {
 	sym_pair *sym_table[MAP_SIZE];
 } sym_table;
 
 sym_table global_table = {{NULL}};
-
 
 typedef struct instruction {
 	const char *mnemonic;
@@ -37,61 +35,56 @@ instruction instruction_set[] = {
 
 int instruction_set_size = (sizeof(instruction_set) / sizeof(instruction_set[0]));
 
-
 unsigned int hash_func(const char *label) {
-    unsigned int hash = 0;
-    while (*label != '\0') {
-        hash = (hash * 31) + *label;
-		label++; 
-    }
-    return hash % MAP_SIZE;
+	unsigned int hash = 0;
+	while (*label != '\0') {
+		hash = (hash * 31) + *label;
+		label++;
+	}
+	return hash % MAP_SIZE;
 }
 
-void print_sym_table(sym_table* table){
+void print_sym_table(sym_table *table) {
 	printf("\nPrinting symbol table:\n");
-	for(int i=0; i<MAP_SIZE;i++){
-		sym_pair* current = table->sym_table[i];
-		while(current!=NULL){
+	for (int i = 0; i < MAP_SIZE; i++) {
+		sym_pair *current = table->sym_table[i];
+		while (current != NULL) {
 			printf("Index: %d, Label: %s, Address: %d\n", i, current->label, current->address);
-            current = current->next;
+			current = current->next;
 		}
 	}
 }
 
-
-
-void insert_label(const char* label, int addr, sym_table* table){
+void insert_label(const char *label, int addr, sym_table *table) {
 	unsigned int index = hash_func(label);
 
-	sym_pair* pair=malloc(sizeof(sym_pair)); // allocate pair to add
-	if(pair==NULL){
+	sym_pair *pair = malloc(sizeof(sym_pair)); // allocate pair to add
+	if (pair == NULL) {
 		perror("Cant allocate mem for new pair");
 		exit(1);
 	}
 
-	pair->label = malloc(strlen(label)+1); // allocate mem for the label char*
-	if(pair->label==NULL){
+	pair->label = malloc(strlen(label) + 1); // allocate mem for the label char*
+	if (pair->label == NULL) {
 		perror("Cant allocate pair to insert's label");
 		exit(1);
 	}
 
-	pair->address = addr;	// update label and insert in hash table (avoid collisions)
+	pair->address = addr;						   // update label and insert in hash table (avoid collisions)
 	memcpy(pair->label, label, strlen(label) + 1); // syn to pair.label = label
-	pair->next= NULL;
+	pair->next = NULL;
 
 	// handle collision
-	sym_pair* current = table->sym_table[index];
-	if(current==NULL){
-		table->sym_table[index]=pair;  // no collision
-	}
-	else{
-		pair->next=current; // allocate mem for the label char*
-		table->sym_table[index]=pair; 
+	sym_pair *current = table->sym_table[index];
+	if (current == NULL) {
+		table->sym_table[index] = pair; // no collision
+	} else {
+		pair->next				= current; // allocate mem for the label char*
+		table->sym_table[index] = pair;
 	}
 
 	printf("Inserted label: %s at address: %d\n:, label, addr");
 }
-
 
 void process_tokens(const char *line) {
 
@@ -112,21 +105,19 @@ void process_tokens(const char *line) {
 	char *token = strtok(line_copy, delimiters);
 
 	while (token != NULL) {
-		
+
 		// check if label
-		if(token[strlen(token)-1] == ':'){
-			token[strlen(token)-1] = '\0';
+		if (token[strlen(token) - 1] == ':') {
+			token[strlen(token) - 1] = '\0';
 			printf("tokens = [%s]\n", token);
-			//populate label table
+			// populate label table
 			insert_label(token, 0, &global_table);
 
-			token = strtok(NULL,delimiters);
+			token = strtok(NULL, delimiters);
 			continue;
-		}
-		else{
+		} else {
 			printf("tokens = [%s]\n", token); // redundant else just for checking printingm will remove later
 		}
-
 
 		int is_instruction = 0;
 		for (int i = 0; i < instruction_set_size; i++) {
@@ -149,7 +140,6 @@ void process_tokens(const char *line) {
 	free(line_copy);
 }
 
-
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		printf("Using: %s <f_name>\n, argv[0]");
@@ -162,11 +152,11 @@ int main(int argc, char *argv[]) {
 		perror("Can't open file for reading");
 		return 1;
 	}
-	
+
 	unsigned char code_seg[MAX_CODE_SIZE];
-	int code_ptr=0;
+	int code_ptr = 0;
 	unsigned char data_seg[MAX_DATA_SIZE];
-	int data_ptr=0;
+	int data_ptr = 0;
 
 	char line[64];
 
